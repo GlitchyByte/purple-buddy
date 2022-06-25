337d4096ea76a14b4e53d0a5afc960e17434832e
Luis Mejia <lmejia@gmail.com>
2022-06-25 07:22:18 -0700

    Version 1.0.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Purple Buddy
+
+![Version](https://img.shields.io/badge/Version-1.0-green)
+
+Chrome extension for [Twitch](https://twitch.com/) auto-collection of channel points and playback resume when errors popup on the video player.
+
+---
+## Installing from source
+
+Clone the repository:
+
+    git clone git@github.com:GlitchyByte/purple-buddy.git
+
+Or [download a zip](https://github.com/GlitchyByte/purple-buddy/archive/refs/heads/main.zip).
+
+Then:
+
+1. Open Chrome.
+2. Go to `Window | Extensions`, make sure `Developer mode` is on.
+3. `Load unpacked` and select the `package` directory.
+
+---
+## How to use
+
+Just click on it to enable the extension (you should see a white `+` over the icon) and your are golden.
+
+No more clicking to collect channel point bonuses. And playback will resume automatically if there is an error with a big red button in the middle of the player waiting for you to hit play.
---
+++ package/setup.js
@@ -0,0 +1,40 @@
+// Copyright 2022 GlitchyByte
+// SPDX-License-Identifier: MIT-0
+
+// Embedded setup.
+
+if (document.querySelector(".tw-animated-glitch-logo") && !window.glitchyByteMainFrameContext) {
+    // Define.
+    class GlitchyByteMainFrameContext {
+
+        constructor() {
+            this.timerId = null
+            const listener = (request, sender, sendResponse) => {
+                switch (request.message) {
+                    case "toggle": return this.toggle(request, sendResponse)
+                }
+            }
+            chrome.runtime.onMessage.addListener(listener.bind(this))
+        }
+
+        toggle(request, sendResponse) {
+            if (this.timerId == null) {
+                this.timerId = setInterval(() => {
+                    const b = document.querySelector("button[aria-label='Claim Bonus']")
+                    if (b) b.click()
+                    const r = document.querySelector("button.ScCoreButton-sc-1qn4ixc-0.ScCoreButtonDestructive-sc-1qn4ixc-4")
+                    if (r) r.click()
+                    const p = document.querySelector("button[aria-label='Play']")
+                    if (p) p.click()
+                }, 5000)
+                sendResponse({ iconOn: true })
+            } else {
+                clearInterval(this.timerId)
+                this.timerId = null
+                sendResponse({ iconOn: false })
+            }
+        }
+    }
+    // Initialize.
+    window.glitchyByteMainFrameContext = new GlitchyByteMainFrameContext()
+}
